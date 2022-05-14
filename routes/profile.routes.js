0767038165fc004e30fb01662ae3af7f045d2998
Mariana Pontes <marianasevinatep@gmail.com>
2022-05-14 13:29:26 +0100

    add friend

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -17,16 +17,48 @@ router.get("/", (req, res, next) => {
 });
 
 /* Edit profile */
-
 router.get("/edit", (req, res, next) => {
   res.render("user/edit-profile");
 });
 
 /* Friends list */
-
 router.get("/friends", (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .populate("friendsList")
+    .then((friends) => {
+      res.render("user/friends-list", { friends });
+    })
+    .catch((err) => next(err));
+  //res.render("user/friends-list");
+});
+
+/* Friends Add- friends*/
+router.get("/add-friend/:id", (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then((userId) => {
+      return User.findByIdAndUpdate(req.session.currentUser, {
+        $push: { friendsList: userId },
+      }).then(() => {
+        res.render("search/friends-details");
+      });
+    })
+    .catch((err) => next(err));
+
   res.render("user/friends-list");
 });
+
+/* Add-Friends-populate */
+/* router.get("/add-friend/:id", (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .populate("friendsList")
+    .then((friends) => {
+      res.render({ friendsList });
+    })
+    .catch((err) => next(err));
+}); */
 
 /* Exports */
 module.exports = router;
