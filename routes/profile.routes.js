64cb4835256c8ad00325262bea98ff76713019c4
Matilde Costa <matildecosta@MBP-de-Matilde.lan>
2022-05-15 09:17:56 +0100

    friends list appeared

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -23,11 +23,11 @@ router.get("/edit", (req, res, next) => {
 
 /* Friends list */
 router.get("/friends", (req, res, next) => {
-  const { id } = req.params;
+  const id = req.session.user._id;
   User.findById(id)
     .populate("friendsList")
-    .then((friends) => {
-      res.render("user/friends-list", { friends });
+    .then((currentUser) => {
+      res.render("user/friends-list", { currentUser });
     })
     .catch((err) => next(err));
   //res.render("user/friends-list");
@@ -38,7 +38,7 @@ router.get("/add-friend/:id", (req, res, next) => {
   const { id } = req.params;
   User.findById(id)
     .then((userId) => {
-      return User.findByIdAndUpdate(req.session.currentUser, {
+      return User.findByIdAndUpdate(req.session.user, {
         $push: { friendsList: userId },
       }).then(() => {
         res.render("search/friends-details");
@@ -48,17 +48,6 @@ router.get("/add-friend/:id", (req, res, next) => {
 
   res.render("user/friends-list");
 });
-
-/* Add-Friends-populate */
-/* router.get("/add-friend/:id", (req, res, next) => {
-  const { id } = req.params;
-  User.findById(id)
-    .populate("friendsList")
-    .then((friends) => {
-      res.render({ friendsList });
-    })
-    .catch((err) => next(err));
-}); */
 
 /* Exports */
 module.exports = router;
